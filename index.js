a878e5fdf8ddedfc29ccab4ea421c5a4d84c8917
Luigi Pinca <luigipinca@gmail.com>
2015-08-17 15:26:07 +0200

    [major] Reduce the length of the generated ids from 8 chars to 7 chars

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
 'use strict';
 
-var seed = 0
+var alphabet = [
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+  'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+  'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+  'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+  'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+  'u', 'v', 'w', 'x', 'y', 'z', '-', '_'
+];
+
+var length = alphabet.length
+  , map = {}
+  , seed = 0
+  , i = 0
   , prev;
+
+/**
+ * Return a string representing the specified number.
+ *
+ * @param {Number} num The number to convert.
+ * @returns {String} The string representation of the number.
+ * @api public
+ */
+function encode(num) {
+  var encoded = '';
+
+  do {
+    encoded = alphabet[num % length] + encoded;
+    num = Math.floor(num / length);
+  } while (num > 0);
+
+  return encoded;
+}
+
+/**
+ * Return the integer value specified by the given string.
+ *
+ * @param {String} str The string to convert.
+ * @returns {Number} The integer value represented by the string.
+ * @api public
+ */
+function decode(str) {
+  var decoded = 0
+    , i = 0;
+
+  while (i < str.length) {
+    decoded = decoded * length + map[str.charAt(i)];
+    i++;
+  }
+
+  return decoded;
+}
 
 /**
  * Yeast: A tiny growing id generator.
@@ -9,9 +60,24 @@ var seed = 0
  * @returns {String} A unique id.
  * @api public
  */
-module.exports = function yeast() {
-  var now = (+new Date()).toString(36);
+function yeast() {
+  var now = encode(+new Date());
 
   if (now !== prev) return seed = 0, prev = now;
-  return now+':'+ (seed++).toString(36);
-};
+  return now +':'+ encode(seed++);
+}
+
+//
+// Map each character to its index.
+//
+while (i < length) {
+  map[alphabet[i]] = i;
+  i++;
+}
+
+//
+// Expose the `yeast`, `encode` and `decode` functions.
+//
+yeast.encode = encode;
+yeast.decode = decode;
+module.exports = yeast;
